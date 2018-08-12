7589105c1a3fd418bcd6373968265516901ef47b
Hachiko <1769969562@qq.com>
2018-08-12 11:50:18 +0800

    [mod]+[add]升级了jetty的版本，解决了数组溢出的warning，设定了用户10分钟之内不访问页面就自动销毁session，前端可以根据用户session的状态去重定向页面，解决了发送邮件给用户之后，用户点击链接无法查看密码的错误。

--- src/main/java/scau/zxck/web/listener/UserSessionListener.java
+++ src/main/java/scau/zxck/web/listener/UserSessionListener.java
@@ -3,64 +3,50 @@ package scau.zxck.web.listener;
 import javafx.scene.control.Alert;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.*;
 import javax.swing.*;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserSessionListener implements HttpSessionListener {
-  private static Map<String,HttpSession> usersessionMap=new HashMap<>();
-  private static Map<String,String> sessionuserMap=new HashMap<>();
+  private static Map<String, HttpSession> usersessionMap = new HashMap<>();
+  private static Map<String, String> sessionuserMap = new HashMap<>();
+
+  public static boolean isAreadyEnter(HttpSession session, String userMseeage, HttpServletResponse response) throws Exception {
+    response.setContentType("text ml; charset=GB2312");
+    PrintWriter out = response.getWriter();
+    boolean flag = false;
+    if (usersessionMap.containsKey(userMseeage)) {
+      flag = true;
+    }
+    if (flag) {
+      sessionuserMap.remove(usersessionMap.remove(userMseeage));
+      usersessionMap.put(userMseeage, session);
+      sessionuserMap.put(session.getId(), userMseeage);
+//            out.print("<script>alert('登录过了还想登录？你是不是癫的？？？');</script>");
+//            out.close();
+      return true;
+    } else {
+      usersessionMap.put(userMseeage, session);
+      sessionuserMap.put(session.getId(), userMseeage);
+      return false;
+    }
+  }
+
+  public static boolean isOnline(HttpSession session) {
+    return sessionuserMap.containsKey(session.getId());
+  }
 
   @Override
   public void sessionCreated(HttpSessionEvent httpSessionEvent) {
-
+    System.out.println("session创建成功！！！！");
   }
 
   @Override
   public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-//        HttpSession session=httpSessionEvent.getSession();
-//        ServletContext application=session.getServletContext();
-//        Map<String,String> userSessionMap=(Map)application.getAttribute("userSessionMap");
-//        if(userSessionMap!=null){
-//            String sessionID=userSessionMap.get((String)session.getAttribute("loginID"));
-//            if(sessionID!=null){
-//                userSessionMap.remove(session.getAttribute("loginID"));
-//            }
-//        }
+    System.out.println("session销毁成功！！！");
     usersessionMap.remove(sessionuserMap.remove(httpSessionEvent.getSession().getId()));
-  }
-  public static boolean isAreadyEnter(HttpSession session, String userMseeage, HttpServletResponse response) throws Exception{
-    response.setContentType("text ml; charset=GB2312");
-    PrintWriter out=response.getWriter();
-    boolean flag=false;
-    int i;
-    String[] strings=userMseeage.split("#");
-    for(i=0;i<strings.length;i++){
-      if(usersessionMap.containsKey(strings[i])){
-        flag=true;
-        break;
-      }
-    }
-    if(flag){
-      sessionuserMap.remove(usersessionMap.remove(strings[i]));
-      usersessionMap.put(userMseeage,session);
-      sessionuserMap.put(session.getId(),userMseeage);
-//            out.print("<script>alert('登录过了还想登录？你是不是癫的？？？');</script>");
-//            out.close();
-      return true;
-    }
-    else{
-      usersessionMap.put(userMseeage,session);
-      sessionuserMap.put(session.getId(),userMseeage);
-      return false;
-    }
-  }
-  public static boolean isOnline(HttpSession session){
-    return sessionuserMap.containsKey(session.getId());
+
   }
 }
