c0d49db984eafa11a4eb06bb8d829f2facdd94a7
Susan-zz <susan_zz@163.com>
2018-08-18 10:29:39 +0800

    files update

--- src/main/webapp/WEB-INF/components/news/moreNewsFather.vue
+++ src/main/webapp/WEB-INF/components/news/moreNewsFather.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="morenews">
-    <news :newlists = "newlists" :pageLists = "pageLists" @switchPage = "switchPage" @lastPage = "lastPage"></news>
+    <news :newlists = "newlists" :pageLists = "pageLists" :nowInPage = "nowInPage" @switchPage = "switchPage" @lastPage = "lastPage" @nextPage = "nextPage"></news>
     <router-view/>
   </div>
 </template>
@@ -14,7 +14,9 @@ export default {
     return {
       newlists: [],
       pageLists: [],
-      page: '1'
+      page: '1',
+      nowInPage: '',
+      pageNum: ''
     }
   },
   components: {
@@ -26,6 +28,7 @@ export default {
       var Page = data
       this.page = data
       axios.post('/api/getBulletin', {Page, NumPerPage}).then(response => {
+        this.nowInPage = response.data[0].NowPage
         for(var j = 0; j < NumPerPage; j++){
           this.newlists.pop()
         }
@@ -42,25 +45,50 @@ export default {
     lastPage: function () {
       var NumPerPage = 2
       var Page = this.page - 1
-      axios.post('/api/getBulletin', {Page, NumPerPage}).then(response => {
-        for(var j = 0; j < NumPerPage; j++){
-          this.newlists.pop()
-        }
-        for (var i = 1; i < response.data.length; i++) {
-          this.newlists.push({
-            title: response.data[i].News_Title,
-            time: response.data[i].News_Time,
-          })
-        }
-      }).catch(function (error) {
-        console.log(error)
-      })
+      if(Page > 0){
+        axios.post('/api/getBulletin', {Page, NumPerPage}).then(response => {
+          this.nowInPage = response.data[0].NowPage
+          for(var j = 0; j < NumPerPage; j++){
+            this.newlists.pop()
+          }
+          for (var i = 1; i < response.data.length; i++) {
+            this.newlists.push({
+              title: response.data[i].News_Title,
+              time: response.data[i].News_Time,
+            })
+          }
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
+    },
+    nextPage: function () {
+      var NumPerPage = 2
+      var Page = this.page + 1
+      if(Page < this.pageNum){
+        axios.post('/api/getBulletin', {Page, NumPerPage}).then(response => {
+          this.nowInPage = response.data[0].NowPage
+          for(var j = 0; j < NumPerPage; j++){
+            this.newlists.pop()
+          }
+          for (var i = 1; i < response.data.length; i++) {
+            this.newlists.push({
+              title: response.data[i].News_Title,
+              time: response.data[i].News_Time,
+            })
+          }
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
     }
   },
   mounted: function() {
       var NumPerPage = 2
       var Page = this.page
       axios.post('/api/getBulletin', {Page, NumPerPage}).then(response => {
+        this.pageNum = response.data[0].PageNum
+        this.nowInPage = response.data[0].NowPage
         for (var i = 1; i < response.data.length; i++) {
           this.newlists.push({
             title: response.data[i].News_Title,
