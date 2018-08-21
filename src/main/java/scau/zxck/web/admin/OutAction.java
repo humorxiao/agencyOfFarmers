15a60ddf5d10585cea982ccef6f3d271c6818aff
cmdmxdz <dzzhn@stu.scau.edu.cn>
2018-08-21 17:57:03 +0800

    [MOD]NOTHING IMPORTANT

--- src/main/java/scau/zxck/web/admin/OutAction.java
+++ src/main/java/scau/zxck/web/admin/OutAction.java
@@ -17,8 +17,18 @@ public class OutAction {
 
   @Autowired
   private HttpSession session;
+  /**
+   * response也可以自动填充，编译器报错是编译器的问题，运行是没有错的，
+   * 每个方法一定要做视图处理，视图处理有多种方式，1）可以在controller方法最后返回一个
+   * 字符串，返回的字符串会在/WEB-INF/pages/目录下找 "返回的字符串"+.jsp这个文件作为视图渲染
+   * 2）可以用response写回各种各样的信息。如果少了视图渲染这步（例如controller方法没有返回值，
+   * 方法里面有没有用response作页面渲染，那么Spring就会默认用controller方法的方法名去/WEN-INF/pages
+   * 目录下找对应的jsp文件，若找不到就会出错
+   */
+  @Autowired
+  private HttpServletResponse response;
   @RequestMapping(value = "out", method = RequestMethod.POST)
-  public void out(HttpServletResponse response)throws Exception{
+  public void out()throws Exception{
     JSONObject temp=new JSONObject();
     if(session.getAttribute("isAdmin")!=null){
       if((boolean)session.getAttribute("isAdmin")){
