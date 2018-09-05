752973001facdb21327a78769866516aa2a18131
huangziqing <848989840@qq.com>
2018-09-05 20:43:10 +0800

    hzqbaba

--- src/main/webapp/WEB-INF/components/customers/Myinfo/infofather.vue
+++ src/main/webapp/WEB-INF/components/customers/Myinfo/infofather.vue
@@ -8,20 +8,42 @@
 
 <script>
 import myinfo1 from './myinfo1.vue'
+import axios from 'axios'
 export default {
   name: 'infofather',
+  mounted:function () {
+     axios.post('/api/getUserDeliveryAddress',{}).then(response => {
+       this.Deliv_Name = response.data.Deliv_Name;
+       this.Deliv_Address = response.data.Deliv_Address;
+       this.Deliv_Cell = response.data.Deliv_Cell;
+       this.Deliv_code = response.data.Deliv_Zipcode;
+     })
+    axios.post('/api/checkLoginRank').then((response) => {
+      // console.log(response.data) // 判断登录状态
+      if (response.data.status === 1) {
+        this.userPK = {'User_PK': response.data.User_PK}
+        axios.post('/api/getUserInfo', this.userPK).then((response) => {
+          console.log(response.data)
+         // this.usesName = response.data.User_Name
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
+    })
+
+  },
   components: {
     'myinfo1': myinfo1
   },
   data () {
     return {
-      username: 'sadsfdfg',
-      name: 'aa',
-      phone: '1111',
-      email: '000',
-      time: '111',
-      id: '111',
-      male: '111',
+      username: '',
+      name: '',
+      phone: '',
+      email: '',
+      time: '',
+      id: '',
+      male: '',
       Deliv_Name: '',
       Deliv_Cell: '',
       Deliv_code: '',
