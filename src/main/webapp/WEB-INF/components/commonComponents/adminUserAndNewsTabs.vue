f3227cac951771aa903fb023a16ed850435c3a77
Mxiaer8 <905594203@qq.com>
2018-09-17 14:28:37 +0800

    add admin_three

---
+++ src/main/webapp/WEB-INF/components/commonComponents/adminUserAndNewsTabs.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <ul class="nav nav-tabs">
+        <li class="active">
+          <a href="#panel-221256" data-toggle="tab">用户黑名单管理</a>
+        </li>
+        <li>
+          <a href="#panel-221257" data-toggle="tab">用户白名单管理</a>
+        </li>
+        <li>
+          <a href="#panel-923724" data-toggle="tab">动态管理</a>
+        </li>
+        <li>
+          <a href="#panel-923725" data-toggle="tab">动态编辑</a>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "adminUserAndNewsTabs"
+    }
+</script>
+
+<style scoped>
+
+</style>
