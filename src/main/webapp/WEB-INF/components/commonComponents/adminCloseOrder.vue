ef4e302c6180aeae8032df65461235173d81280a
Susan-zz <susan_zz@163.com>
2018-09-18 20:55:43 +0800

    adminOrder updates

---
+++ src/main/webapp/WEB-INF/components/commonComponents/adminCloseOrder.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal fade" id="modal-container-Order" role="dialog" aria-hidden="true" aria-labelledby="myModalLabel">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button class="close" aria-hidden="true" type="button"
+                  data-dismiss="modal">×</button>
+        </div>
+        <div class="modal-body" id="order">
+
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-default" type="button" data-dismiss="modal">关闭</button>
+          <span id="Order"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "closeOrder"
+  }
+</script>
+
+<style scoped>
+
+</style>
