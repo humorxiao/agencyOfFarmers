010393dabebd4fd817425e4c1acf640f44311bd9
huangziqing <848989840@qq.com>
2018-09-19 23:35:29 +0800

    src/main/webapp/WEB-INF/components/admin/four_unionfather.vue src/main/webapp/WEB-INF/components/admin/one_unionfather.vue src/main/webapp/WEB-INF/components/admin/three_unionfather.vue src/main/webapp/WEB-INF/components/admin/two_unionfather.vue src/main/webapp/WEB-INF/components/commonComponents/four_union.vue src/main/webapp/WEB-INF/components/commonComponents/one_union.vue src/main/webapp/WEB-INF/components/commonComponents/three_union.vue src/main/webapp/WEB-INF/components/commonComponents/two_union.vue src/main/webapp/WEB-INF/components/commonComponents/union_all.vue src/main/webapp/WEB-INF/components/commonComponents/unionmanageNavar.vue src/main/webapp/WEB-INF/components/ico-img/add.jpg src/main/webapp/static/image/add.jpg src/main/webapp/WEB-INF/components/customers/Myinfo/myinfo1.vue src/main/webapp/WEB-INF/components/unionManagePage.vue

---
+++ src/main/webapp/WEB-INF/components/commonComponents/one_union.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+      <div class="tab-pane active" id="panel-221256">
+        <br>
+        <el-input v-model="input" placeholder="搜索合作社（名称、负责人、地址、负责人电话）"  width="20%"></el-input>
+
+     <a class="btn" href="#modal-container-Union" @click=addUnion1()
+          data-toggle="modal"><img style="width: 25px;"
+           src="../ico-img/add.jpg">添加合作社</a> <br><br>
+        <el-table
+          :data="message"
+          style="width: 100%"
+          max-height="510">
+          <el-table-column
+            fixed
+            prop="union_name"
+            label="合作社名称"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="user_name"
+            label="负责人姓名/法人"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="id"
+            label="营业执照号码"
+            width="250">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址/住所"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="time"
+            label="成立日期"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="sum"
+            label="出资总额"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="fixphone"
+            label="固定电话"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="phone"
+            label="负责人手机"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="email"
+            label="电子邮件"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作一"
+            width="120">
+            <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作二"
+            width="120">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <span id="User-msg" style="color:red"></span><br>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "one_union",
+      props:{
+       message:{
+         type:Array,
+         required: true
+       }
+      },
+      data(){
+          return{
+            input:''
+          }
+      }
+    }
+</script>
+
+<style scoped>
+  .el-input{
+    width: 20%;
+  }
+</style>
--- src/main/webapp/WEB-INF/components/unionManagePage.vue
+++ src/main/webapp/WEB-INF/components/unionManagePage.vue
@@ -1,10 +1,50 @@
 <template>
-  <h1>合作社内部信息</h1>
+  <div>
+    <navar></navar>
+  <div class="row clearfix div-bg-color">
+    <div class="col-md-10 col-md-offset-1 column">
+      <div class="tabbable" id="tabs-952416">
+        <br><unionall></unionall>
+        <div class="tab-content div-bg-color">
+          <div class="tab-pane active" id="panel-221256">
+          <one></one>
+          </div>
+          <div class="tab-pane" id="panel-923725">
+          <two></two>
+          </div>
+          <div class="tab-pane" id="panel-923726">
+            <three></three>
+          </div>
+          <div class="tab-pane" id="panel-923727">
+            <four></four>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ <adminfooter></adminfooter>
+ </div>
 </template>
 
 <script>
+  import unionall from './commonComponents/union_all.vue'
+  import one from './admin/one_unionfather.vue'
+  import navar from './commonComponents/unionmanageNavar.vue'
+  import adminfooter from './commonComponents/adminfooter.vue'
+  import two from './admin/two_unionfather'
+  import three from './admin/three_unionfather'
+  import four from './admin/four_unionfather'
     export default {
-        name: "unionManagePage"
+        name: "unionManagePage",
+      components:{
+          unionall,
+        one,
+        navar,
+        adminfooter,
+        two,
+        three,
+        four
+      }
     }
 </script>
 
