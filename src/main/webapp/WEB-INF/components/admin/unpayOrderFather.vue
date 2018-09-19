056b14c54006ec08a2e2ee9ee40dc33160cb8eba
Susan-zz <susan_zz@163.com>
2018-09-19 23:37:08 +0800

    adminOrderPart upate again

--- src/main/webapp/WEB-INF/components/admin/unpayOrderFather.vue
+++ src/main/webapp/WEB-INF/components/admin/unpayOrderFather.vue
@@ -87,6 +87,22 @@ export default {
         console.log(error);
       })
     }
+  },
+  mounted: function () {
+    var stste = {'Order_State':1}
+    axios.post('/api/getStateOrderPaging',stste).then(response =>{
+      for(var i = 0; i < response.data.length; i++){
+        this.tableOrder.push({
+          orderID: response.data[i].Order_ID,
+          userPK: response.data[i].User_PK,
+          orderTime: response.data[i].Order_Time,
+          orderPayPrice: response.data[i].Order_PayPrice
+        })
+      }
+
+    }).catch(function (error) {
+      console.log(error)
+    })
   }
 }
 
