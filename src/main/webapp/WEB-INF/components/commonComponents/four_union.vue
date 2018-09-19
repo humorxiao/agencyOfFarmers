441e56ee6378dd2401a151a512c928fce0f9bf42
huangziqing <848989840@qq.com>
2018-09-19 23:35:29 +0800

    src/main/webapp/WEB-INF/components/admin/four_unionfather.vue src/main/webapp/WEB-INF/components/admin/one_unionfather.vue src/main/webapp/WEB-INF/components/admin/three_unionfather.vue src/main/webapp/WEB-INF/components/admin/two_unionfather.vue src/main/webapp/WEB-INF/components/commonComponents/four_union.vue src/main/webapp/WEB-INF/components/commonComponents/one_union.vue src/main/webapp/WEB-INF/components/commonComponents/three_union.vue src/main/webapp/WEB-INF/components/commonComponents/two_union.vue src/main/webapp/WEB-INF/components/commonComponents/union_all.vue src/main/webapp/WEB-INF/components/commonComponents/unionmanageNavar.vue src/main/webapp/WEB-INF/components/ico-img/add.jpg src/main/webapp/static/image/add.jpg src/main/webapp/WEB-INF/components/customers/Myinfo/myinfo1.vue src/main/webapp/WEB-INF/components/unionManagePage.vue

---
+++ src/main/webapp/WEB-INF/components/commonComponents/four_union.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="tab-pane" id="panel-923727">
+      <br>
+      <el-input v-model="input" placeholder="搜索农产品"  width="20%"></el-input>
+      <a class="btn" href="#modal-container-Prod" @click=addProd1()
+         data-toggle="modal"><img style="width: 25px;"
+          src="../ico-img/add.jpg">新增农产品</a> <br><br>
+      <el-table
+        :data="message"
+        style="width: 100%"
+        max-height="510">
+        <el-table-column
+          prop="goodsname"
+          label="产品名称"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="goodstype"
+          label="类别"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="season"
+          label="一年几季"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="flower"
+          label="开花期"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="fruit"
+          label="挂果期"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="grownup"
+          label="成熟期"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop="fresh"
+          label="保质期"
+          width="150">
+        </el-table-column>
+        <el-table-column
+          label="操作一"
+          width="120">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作二"
+          width="120">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <span id="User-msg" style="color:red"></span><br>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "four_union",
+      props:{
+        message:{type:Array,required:true}
+      },
+      data(){
+        return{
+          input:''
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
