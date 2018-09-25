bcfeb3f58c836f17cad22b3e86e6ae154e908b60
Mxiaer8 <905594203@qq.com>
2018-09-25 17:22:36 +0800

    mod admin

--- src/main/webapp/WEB-INF/components/commonComponents/four_adminUserAndNews.vue
+++ src/main/webapp/WEB-INF/components/commonComponents/four_adminUserAndNews.vue
@@ -8,25 +8,35 @@
       </el-form-item>
       </el-form>
       </div>
-      <editorfather></editorfather>
+
+     <div id="vueapp">
+      <quill-editor v-model="content"
+                    ref="quillEditorA"
+                    :options="editorOption"
+                    @blur="onEditorBlur($event)"
+                    @focus="onEditorFocus($event)"
+                    @ready="onEditorReady($event)">
+      </quill-editor>
+      <div class="content ql-editor" v-html="content"></div>
+    </div>
+
       <div>
-      <el-button type="primary">提交</el-button>
+      <el-button type="primary" @click = "tosubmit()">提交</el-button>
       </div>
   </div>
 
 </template>
 
 <script>
-import myeditorfather from '../admin/editorFather.vue'
     export default {
-        // name: "t_adminUserAndNews",
       components: {
-          editorfather: myeditorfather
+          LocalQuillEditor: VueQuillEditor.quillEditor
       },
       data() {
           return{
             value: '',
             input: '',
+            content: '',
             ruleForm: {
               name: ''
             },
@@ -35,8 +45,53 @@ import myeditorfather from '../admin/editorFather.vue'
                 { required: true, message: '请输入新闻标题', trigger: 'blur' },
                 { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
               ]
+            },
+            message: '',
+            // content: '<h2>I am Example A</h2>',
+            // content2: '<h2>I am Example B</h2>',
+            editorOption: {
+              theme: 'snow'
             }
           }
+      },
+      methods: {
+        onEditorBlur(quill) {
+          console.log('editor blur!', quill)
+        },
+        onEditorFocus(quill) {
+          console.log('editor focus!', quill)
+        },
+        onEditorReady(quill) {
+          console.log('editor ready!', quill)
+        },
+        tosubmit() {
+          console.log(this.content)
+          console.log(this.ruleForm.name)
+          var time = this.gettime()
+          console.log(time)
+        },
+        gettime() {
+          var myDate = new Date();//获取系统当前时间
+          // console.log(myDate.getFullYear()) //获取完整的年份(4位,1970-????)
+          // console.log(myDate.getMonth()) //获取当前月份(0-11,0代表1月)
+          // console.log(myDate.getDate()) //获取当前日(1-31)
+          // console.log(myDate.getHours())// 时
+          // console.log( myDate.getMinutes()) //分
+          // console.log( myDate.getSeconds()) //秒
+          var time = myDate.getFullYear() + '-' + (myDate.getMonth()+1) + '-' + myDate.getDate() + ' ' + myDate.getHours() + ':' +  myDate.getMinutes() + ':' + myDate.getSeconds()
+          return time
+        }
+      },
+      computed: {
+        editorA() {
+          return this.$refs.quillEditorA.quill
+        },
+        editorB() {
+          return this.$refs.quillEditorB.quill
+        }
+      },
+      mounted() {
+        console.log('this is quill A instance object', this.editorA, 'B instance', this.editorB)
       }
     }
 </script>
@@ -47,5 +102,9 @@ import myeditorfather from '../admin/editorFather.vue'
   }
   .title_table{
     margin-left: -45px;
+  }
+  .quill-editor,
+  .content {
+    background-color: white;
   }
 </style>
