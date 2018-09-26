4e48a5d0b20f27ebab23abb158f661d338b121d5
huangziqing <848989840@qq.com>
2018-09-26 23:09:56 +0800

    new

--- src/main/webapp/WEB-INF/components/commonComponents/two_union.vue
+++ src/main/webapp/WEB-INF/components/commonComponents/two_union.vue
@@ -67,54 +67,54 @@
       <span id="User-msg" style="color:red"></span><br>
     </div>
     <!--添加新产品-->
-    <!--<div class="modal fade" id="modal-container-Goods" role="dialog"-->
-         <!--aria-hidden="true" aria-labelledby="myModalLabel">-->
-      <!--<div class="modal-dialog">-->
-        <!--<div class="modal-content">-->
-          <!--<div class="modal-header">-->
-            <!--<button class="close" aria-hidden="true" type="button"-->
-                    <!--data-dismiss="modal">×</button>-->
-            <!--<h4 class="modal-title" id="myModalLabel">商品资料填写</h4>-->
-          <!--</div>-->
-          <!--<div class="modal-body">-->
-          <!--<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="50px" class="demo-ruleForm">-->
-            <!--<el-form-item label="商品名称" prop="name">-->
-              <!--<el-input v-model="ruleForm.name"></el-input>-->
-            <!--</el-form-item>-->
-            <!--<el-form-item label="商品类别" prop="type">-->
-              <!--<el-select v-model="ruleForm.type" placeholder="请选择商品类别">-->
-                <!--<el-option label="水果" value="1"></el-option>-->
-                <!--<el-option label="加工品" value="2"></el-option>-->
-                <!--<el-option label="粮疏" value="3"></el-option>-->
-                <!--<el-option label="水产" value="4"></el-option>-->
-                <!--<el-option label="禽畜" value="5"></el-option>-->
-                <!--<el-option label="非水果粮蔬农作物" value="6"></el-option>-->
-              <!--</el-select>-->
-            <!--</el-form-item>-->
-            <!--<el-form-item label="商品库存" prop="leftnum">-->
-              <!--<el-input v-model="ruleForm.leftnum"></el-input>-->
-            <!--</el-form-item>-->
-            <!--<el-form-item label="商品价格" prop="price">-->
-              <!--<el-input v-model="ruleForm.price"></el-input>-->
-            <!--</el-form-item>-->
-            <!--<el-form-item label="商品是否售卖" prop="sales">-->
-              <!--<el-radio-group v-model="ruleForm.sales">-->
-                <!--<el-radio label="是" value="0"></el-radio>-->
-                <!--<el-radio label="否" value="1"></el-radio>-->
-              <!--</el-radio-group>-->
-            <!--</el-form-item>-->
-            <!--<el-form-item label="商品展示位置" prop="type">-->
-              <!--<el-select v-model="ruleForm.type" placeholder="请选择商品类别">-->
-                <!--<el-option label="特色区" value="1"></el-option>-->
-                <!--<el-option label="优惠区" value="2"></el-option>-->
-                <!--<el-option label="无展示" value="3"></el-option>-->
-              <!--</el-select>-->
-            <!--</el-form-item>-->
-          <!--</el-form>-->
-          <!--</div>-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
+    <div class="modal fade" id="modal-container-Goods" role="dialog"
+         aria-hidden="true" aria-labelledby="myModalLabel">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button class="close" aria-hidden="true" type="button"
+                    data-dismiss="modal">×</button>
+            <h4 class="modal-title" id="myModalLabel">商品资料填写</h4>
+          </div>
+          <div class="modal-body">
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="50px" class="demo-ruleForm">
+            <el-form-item label="商品名称" prop="name">
+              <el-input v-model="ruleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品类别" prop="type">
+              <el-select v-model="ruleForm.type" placeholder="请选择商品类别">
+                <el-option label="水果" value="1"></el-option>
+                <el-option label="加工品" value="2"></el-option>
+                <el-option label="粮疏" value="3"></el-option>
+                <el-option label="水产" value="4"></el-option>
+                <el-option label="禽畜" value="5"></el-option>
+                <el-option label="非水果粮蔬农作物" value="6"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="商品库存" prop="leftnum">
+              <el-input v-model="ruleForm.leftnum"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="price">
+              <el-input v-model="ruleForm.price"></el-input>
+            </el-form-item>
+            <el-form-item label="商品是否售卖" prop="sales">
+              <el-radio-group v-model="ruleForm.sales">
+                <el-radio label="是" value="0"></el-radio>
+                <el-radio label="否" value="1"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="商品展示位置" prop="type">
+              <el-select v-model="ruleForm.type" placeholder="请选择商品类别">
+                <el-option label="特色区" value="1"></el-option>
+                <el-option label="优惠区" value="2"></el-option>
+                <el-option label="无展示" value="3"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 </template>
 
