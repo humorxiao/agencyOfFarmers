026c6d7d82096ad9f8e97a340d0f5d2002d07487
huangziqing <848989840@qq.com>
2018-09-27 21:46:09 +0800

    927

--- src/main/webapp/WEB-INF/components/customers/Historyorders/orders.vue
+++ src/main/webapp/WEB-INF/components/customers/Historyorders/orders.vue
@@ -13,72 +13,27 @@
         <div class="tab-content div-bg-color">
           <div class="tab-pane active" id="panel-923722">
             <div class="col-md-8 col-md-offset-1 column" id="Orders8">
-              <allorder :goods_list="this.goods"></allorder>
-            </div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(8)">上一页</a></li>
-                <li><a class="btn" id="orders-page8">1</a></li>
-                <li><a class="btn" onclick="add(8)">下一页</a></li>
-                <li><a class="btn" id="page8">共1页</a></li>
-              </ul>
+              <allorder :tableData5="tableData5"></allorder>
             </div>
           </div>
           <div class="tab-pane" id="panel-923723">
-            <div class="col-md-8 col-md-offset-1 column" id="Orders1"></div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(1)">上一页</a></li>
-                <li><a class="btn" id="orders-page1">1</a></li>
-                <li><a class="btn" onclick="add(1)">下一页</a></li>
-                <li><a class="btn" id="page1">共1页</a></li>
-              </ul>
+            <div class="col-md-8 col-md-offset-1 column" id="Orders1">
+           <nopay></nopay>
             </div>
           </div>
           <div class="tab-pane" id="panel-923724">
             <div class="col-md-8 col-md-offset-1 column" id="Orders2">
               <nodeliver></nodeliver>
             </div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(2)">上一页</a></li>
-                <li><a class="btn" id="orders-page2">1</a></li>
-                <li><a class="btn" onclick="add(2)">下一页</a></li>
-                <li><a class="btn" id="page2">共1页</a></li>
-              </ul>
-            </div>
           </div>
           <div class="tab-pane" id="panel-923725">
-            <div class="col-md-8 col-md-offset-1 column" id="Orders3"></div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(3)">上一页</a></li>
-                <li><a class="btn" id="orders-page3">1</a></li>
-                <li><a class="btn" onclick="add(3)">下一页</a></li>
-                <li><a class="btn" id="page3">共1页</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="tab-pane" id="panel-923726">
-            <div class="col-md-8 col-md-offset-1 column" id="Orders4"></div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(4)">上一页</a></li>
-                <li><a class="btn" id="orders-page4">1</a></li>
-                <li><a class="btn" onclick="add(4)">下一页</a></li>
-                <li><a class="btn" id="page4">共1页</a></li>
-              </ul>
+            <div class="col-md-8 col-md-offset-1 column" id="Orders3">
+              <nosign></nosign>
             </div>
           </div>
           <div class="tab-pane" id="panel-923727">
-            <div class="col-md-8 col-md-offset-1 column" id="Orders5"></div>
-            <div class="col-md-10 column">
-              <ul class="pagination pull-right">
-                <li><a class="btn" onclick="minus(5)">上一页</a></li>
-                <li><a class="btn" id="orders-page5">1</a></li>
-                <li><a class="btn" onclick="add(5)">下一页</a></li>
-                <li><a class="btn" id="page5">共1页</a></li>
-              </ul>
+            <div class="col-md-8 col-md-offset-1 column" id="Orders5">
+              <finish></finish>
             </div>
           </div>
         </div>
@@ -88,16 +43,85 @@
 </template>
 
 <script>
+  import axios from 'axios'
   import allorder from './allorder.vue'
   import nodeliver from './nodeliveryorder.vue'
+  import nopay from './nopayorders'
+  import nosign from './nosignorder'
+  import finish from './finishorder'
 export default {
   name: 'orders',
-  components: {allorder,nodeliver},
+  components: {allorder,nodeliver,nopay,nosign,finish},
   data () {
     return {
-      goods:[{orderid:1000,ordertime:1000 },
-        {orderid:2000,ordertime:100}]
+      tableData5: [{
+        id: '12987122',
+       ordertime:'2013',
+       paytime:'2016-52',
+        status:'未支付',
+        money:'5000',
+        goods:[{name:'xxx',price:'11111',sum:'500'}]
+      }],
+      status:'',
+      goods:[]
     }
+  },
+  mounted:function () {
+    axios.post('/api/getUserOrderListPaging', {}).then(response => {
+      for (let i = 0; i < response.data.length; i++) {
+        if(response.data[i].Order_State === 1)
+        {this.status = '已下订单未支付'}
+        else if(response.data[i].Order_State === 2)
+        {this.status = '取消'}
+        else if(response.data[i].Order_State === 3)
+        {this.status='卖家已发货，快递正在路上'}
+        else if(response.data[i].Order_State === 4)
+        {this.status='用户已签收'}
+        else if(response.data[i].Order_State === 5)
+        {this.status='用户已确认收货，订单完成'}
+        else {this.status='已支付未发货'}
+        var goodsnameall = response.data[i].Goods_List
+       var goodsname = goodsnameall.split("#")
+       // this.goods=[]
+       // for(let j = 0;j < goodsname.length-1;j++)
+       // { var data={"Goods_PK":goodsname[j].toString()}
+       //    console.log(goodsname[j])
+         // axios.post('/api/getOneGood', data).then(response1 => {
+         //   var num = response1.data.Goods_Price*response1.data.Goods_Num
+         //   console.log(response1.data)
+           // this.goods.push({
+           //   name:response1.data.Goods_Name,
+           //   price:response1.data.Goods_Price.toString(),
+           //   sum:num.toString()
+           // })
+       //   })
+       // }
+        //console.log(this.goods)
+        console.log('////////////////////')
+        this.tableData5.push({
+          id:response.data[i].Order_ID,
+          ordertime:response.data[i].Order_Time,
+          paytime:response.data[i].Order_PayTime,
+          status:this.status,
+          money:response.data[i].Order_PayPrice,
+          goods_list:goodsname
+        })
+        console.log(this.tableData5)
+      }
+    })
+     for(let i = 0;i<this.tableData5.length;i++) {
+      console.log('///////')
+       for (let j = 0; j < this.tableData5[i].goods_list.length - 1; j++) {
+         console.log(this.tableData5[i].goods_list[j])
+         var data = {"Goods_PK": this.tableData5[i].goods_list[j]}
+         axios.post('/api/getOneGood', data).then(response1 => {
+           console.log(response1.data)
+           // this.tableData5.push({
+           //   goods:
+           // })
+         })
+       }
+     }
   },
   methods: {
 
--- src/main/webapp/WEB-INF/history.js
+++ src/main/webapp/WEB-INF/history.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import History from './History.vue'
 import router from './router/history.js'
-
+import {Button,Message,  Table, TableColumn,  Popover, Tag, Input, FormItem,Form , Select,
+  Option,MessageBox} from 'element-ui'
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Popover)
+Vue.use(Button)
+Vue.use(Tag)
+Vue.use(Input)
+Vue.use( FormItem)
+Vue.use(Form)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(VueQuillEditor)
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$message = Message;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
 new Vue({
   el: '#history',
   router,
