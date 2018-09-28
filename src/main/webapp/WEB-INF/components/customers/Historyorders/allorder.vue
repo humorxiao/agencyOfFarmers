157710a500282c97d2b3f2c517c61cca2076bfca
huangziqing <848989840@qq.com>
2018-09-28 22:21:47 +0800

    消费者

--- src/main/webapp/WEB-INF/components/customers/Historyorders/allorder.vue
+++ src/main/webapp/WEB-INF/components/customers/Historyorders/allorder.vue
@@ -3,7 +3,8 @@
     <div  id="Orders8">
       <el-table
         :data="tableData5"
-        style="width: 100%">
+        style="width: 100%"
+        max-height="600">
         <el-table-column type="expand">
           <template slot-scope="props">
             <el-form label-position="left" inline class="demo-table-expand" >
@@ -19,6 +20,10 @@
                   prop="price"
                   label="单价"
                   width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="number"
+                  label="数量">
                 </el-table-column>
                 <el-table-column
                   prop="sum"
@@ -53,27 +58,7 @@
           prop="money"
           wide="150">
         </el-table-column>
-        <el-table-column label="评价">
-          <template slot-scope="scope">
-            <el-button
-              type="info"
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">添加评价</el-button>
-          </template>
-        </el-table-column>
       </el-table>
-      <!--<hr size="55px" noshade="true"><span class="pull-left">订单号：{{good.orderid}}-->
-      <!--<br>下单时间:{{good.ordertime}}<br>订单支付时间:{{good.paytime}}</span>-->
-      <!--<span class="pull-right">订单状态:{{good.orderstate}}<br>交易额:{{good.sum}}元</span>-->
-      <!--<table class="table"><thead><tr>-->
-        <!--<th>商品名</th>-->
-        <!--<th>单价</th>-->
-        <!--<th>数量</th>-->
-        <!--<th>小计</th>-->
-        <!--<th>评价</th>-->
-      <!--</tr></thead><tbody>-->
-      <!--<tr><td>{{name}}</td><td>{{price}}</td><td>{{num}}</td><td>{{count}}</td></tr>-->
-      <!--</tbody></table>-->
     </div>
   </div>
 </template>
@@ -84,16 +69,6 @@ export default {
   name: 'allorder',
   props: {
     tableData5: {type: Array, required: true},
-  },
-  components: {
-  },
-  data () {
-    return {
-      name:'',
-      price:'',
-      num:'',
-      count:''
-    }
   }
 }
 </script>
--- src/main/webapp/WEB-INF/components/customers/Historyorders/nosignorder.vue
+++ src/main/webapp/WEB-INF/components/customers/Historyorders/nosignorder.vue
@@ -1,21 +1,82 @@
 <template>
-    <div class="tab-pane" id="panel-923725">
-      <br>
-      <div class="col-md-8 col-md-offset-1 column" id="Orders3"></div>
-      <div class="col-md-12 column">
-        <ul class="pagination pull-right">
-          <li><a class="btn" onclick="minus(3)">上一页</a></li>
-          <li><a class="btn" id="orders-page3">1</a></li>
-          <li><a class="btn" onclick="add(3)">下一页</a></li>
-          <li><a class="btn" id="page3">共1页</a></li>
-        </ul>
+      <div  id="Orders3">
+        <el-table
+          :data="tableData3"
+          style="width: 100%">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-form label-position="left" inline class="demo-table-expand" >
+                <el-table
+                  :data="props.row.goods"
+                  style="width: 100%">
+                  <el-table-column
+                    prop="name"
+                    label="商品名"
+                    width="180">
+                  </el-table-column>
+                  <el-table-column
+                    prop="price"
+                    label="单价"
+                    width="180">
+                  </el-table-column>
+                  <el-table-column
+                    prop="number"
+                    label="数量">
+                  </el-table-column>
+                  <el-table-column
+                    prop="sum"
+                    label="小计">
+                  </el-table-column>
+                </el-table>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="订单号"
+            prop="id"
+            wide="300">
+          </el-table-column>
+          <el-table-column
+            label="下单时间"
+            prop="ordertime"
+            wide="250">
+          </el-table-column>
+          <el-table-column
+            label="订单支付时间"
+            prop="paytime"
+            wide="250">
+          </el-table-column>
+          <el-table-column
+            label="订单状态"
+            prop="status"
+            wide="150">
+          </el-table-column>
+          <el-table-column
+            label="交易额"
+            prop="money"
+            wide="150">
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button
+                type="warning"
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">确认收货</el-button>
+              <el-button type="danger"
+                         size="mini"
+                         @click="checkdelivery(scope.$index, scope.row)">查看物流</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
       </div>
-    </div>
 </template>
 
 <script>
 export default {
-  name: 'nosignorder'
+  name: 'nosignorder',
+  props: {
+    tableData3: {type: Array, required: true},
+  }
 }
 </script>
 
