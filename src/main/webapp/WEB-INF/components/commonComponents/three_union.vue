7581366ab5593bed232302469154d63020a334ab
Susan-zz <susan_zz@163.com>
2018-10-01 11:08:09 +0800

    10.01 _1

--- src/main/webapp/WEB-INF/components/commonComponents/three_union.vue
+++ src/main/webapp/WEB-INF/components/commonComponents/three_union.vue
@@ -3,7 +3,7 @@
     <br>
     <el-input v-model="input" placeholder="搜索合作社人员"  style="width:20%" clearable></el-input>
     <el-button type="primary" icon="el-icon-search" @click="searchUnion(input)">搜索</el-button>
-    <el-button type="text" style="font-size: large">新增合作社成员</el-button>
+    <el-button type="text" style="font-size: large"@click="addStaff('form')">新增合作社成员</el-button>
     <el-table
       :data="message"
       style="width: 100%"
@@ -94,43 +94,48 @@
       </el-table-column>
     </el-table>
 
-    <el-dialog title="合作社人员信息" :visible.sync="editFormVisible" width="25%">
-      <el-form :model="form" size="medium">
-        <el-form-item label="姓名" :label-width="formLabelWidth">
+    <el-dialog title="合作社人员信息" :visible.sync="editFormVisible" width="30%">
+      <el-form :model="form" :rules="rules" ref="ruleForm" size="medium" class="demo-ruleForm">
+        <el-form-item label="姓名" :label-width="formLabelWidth" prop="name">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="姓名" :label-width="formLabelWidth">
+        <el-form-item label="性别" :label-width="formLabelWidth" prop="male">
           <el-select v-model="form.male" placeholder="性别">
             <el-option label="男" value="man"></el-option>
             <el-option label="女" value="woman"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="出生日期" :label-width="formLabelWidth">
+        <el-form-item label="出生日期" :label-width="formLabelWidth" required>
+          <el-col :span="11">
           <el-date-picker
             v-model="form.birthday"
             type="date"
+            value-format=" yyyy-MM-dd" format="yyyy-MM-dd"
             placeholder="选择日期">
           </el-date-picker>
+          </el-col>
         </el-form-item>
-        <el-form-item label="地址" :label-width="formLabelWidth">
+        <el-form-item label="地址" :label-width="formLabelWidth" prop="address">
           <el-input v-model="form.address"></el-input>
         </el-form-item>
-        <el-form-item label="电话" :label-width="formLabelWidth">
+        <el-form-item label="电话" :label-width="formLabelWidth" prop="phone">
           <el-input v-model="form.phone"></el-input>
         </el-form-item>
-        <el-form-item label="身份证号码" :label-width="formLabelWidth">
+        <el-form-item label="身份证号码" :label-width="formLabelWidth" prop="id">
           <el-input v-model="form.id"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱地址" :label-width="formLabelWidth">
+        <el-form-item label="邮箱地址" :label-width="formLabelWidth" prop="email">
           <el-input v-model="form.email"></el-input>
         </el-form-item>
-        <el-form-item label="所属合作社" :label-width="formLabelWidth">
-          <el-input v-model="form.union"></el-input>
+        <el-form-item label="所属合作社" :label-width="formLabelWidth" prop="union">
+          <el-select v-model="form.union" placeholder="所属合作社">
+            <el-option v-for="Item in unionList" :key="Item.id" :value="Item.Uname">{{Item.Uname}}</el-option>
+          </el-select>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="editFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="sure('form')">确 定</el-button>
       </div>
     </el-dialog>
     <span id="User-msg" style="color:red"></span><br>
@@ -141,10 +146,43 @@
     export default {
         name: "three_union",
       props:{
-        message:{type:Array,required:true}
+        message:{
+          type:Array,
+          required:true
+        },
+        unionList:{
+          type: Array,
+          required: true
+        }
       },
       data(){
         return{
+          rules:{
+            name: [{
+              required: true, message: '请输入成员的名字', trigger: 'blur'
+            }],
+            male: [{
+              required: true,message:'请选择成员的性别',trigger:'change'
+            }],
+            birthday: [{
+              type: 'string', required: true, message: '请选择日期', trigger: 'change'
+            }],
+            address: [{
+              required: true,message:'请填写成员的地址',trigger:'blur'
+            }],
+            phone: [{
+              required: true,message:'请填写成员的联系电话',trigger:'blur'
+            }],
+            id: [{
+              required: true,message:'请填写成员的身份证号码',trigger:'blur'
+            }],
+            email: [{
+              required: true,message:'请填写成员的邮箱',trigger:'blur'
+            }],
+            union: [{
+              required: true,message:'请选择成员的所属合作社',trigger:'change'
+            }],
+          },
           input:'',
           editFormVisible: false,
           form: {
@@ -157,7 +195,7 @@
             email: '',
             union: '',
           },
-          formLabelWidth: '100px'
+          formLabelWidth: '100px',
         }
       },
       methods: {
@@ -175,10 +213,47 @@
           this.form.email = row.email;
           this.form.union = row.union;
         },
-//        addStaff: function () {
-//          this.editFormVisible=true;
-//          this.$emit('addStaff',this.form.name)
-//        }
+        sure: function (formName) {
+          this.form.name = row.name;
+          this.form.male = row.male;
+          this.form.birthday = row.birthday;
+          this.form.address = row.address;
+          this.form.phone = row.phone;
+          this.form.id = row.id;
+          this.form.email = row.email;
+          this.form.union = row.union;
+          this.editFormVisible = false
+        },
+        addStaff: function (formName) {
+          this.editFormVisible=true;
+          this.$refs[formName].validate((valid) => {
+            if (valid) {
+              alert('submit!');
+            } else {
+              console.log('error submit!!');
+              return false;
+            }
+          });
+
+          this.$emit('addStaff',this.form.name)
+        },
+        handleDelete: function (index,row) {
+          this.$confirm('此操作将删除该成员, 是否继续?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+          }).then(() => {
+            this.$message({
+              type: 'success',
+              message: '删除成功'
+            });
+            this.$emit('handleDelete',index,row)
+          }).catch(() => {
+            this.$message({
+              type: 'info',
+              message: '取消操作'
+            });
+          });
+        }
       }
     }
 </script>
